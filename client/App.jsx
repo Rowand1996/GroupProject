90b16d3bbc04ff689b2ece132f2c184889cdbc2a
Alandis Seals <alandis.seals@yahoo.com>
2020-08-06 13:47:25 -0500

    Add search bar to map and styled it

--- client/App.jsx
+++ client/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import HomePage from './components/HomePage.jsx';
 import Symptoms from './components/Symptoms.jsx';
 import statePage from './components/statePage.jsx';
+import Map from './components/Map.jsx'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 // import $ from 'jquery';
@@ -18,6 +19,7 @@ const App = () => {
               <Route exact path='/' component={HomePage}/>
               <Route exact path='/symptoms' component={Symptoms} />
               <Route path='/state/:id' component={statePage} />
+              <Route path='/map' component={Map}/>
             </Switch>
           </Router>
         
